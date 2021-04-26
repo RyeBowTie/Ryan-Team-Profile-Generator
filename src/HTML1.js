35df5e9be51b6d3a035cd0adffbde796e22b24f8
Ryan O'Donohue <rod.odonohue@gmail.com>
2021-04-26 16:30:01 -0400

    moved html template files to their own folder.

---
+++ src/HTML1.js
@@ -0,0 +1,17 @@
+module.exports = 
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="./reset.css">
+<link href="https://fonts.googleapis.com/css2?family=Coda+Caption:wght@800&family=Noto+Serif+TC:wght@600&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="./style.css">
+<title>Team Profile Generator</title>
+</head>
+<body>
+    <header>
+        <h1>TEAM PROFILE</h1>
+    </header>
+    <main>`
